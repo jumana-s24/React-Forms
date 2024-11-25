5d264a80e922a9f55080ac1a8a412b5d5d3eaf0b
jsalhab <salhab.jumana24@gmail.com>
2024-11-25 19:28:00 +0100

    Fix a style issue

--- src/components/ui/SelectField/SelectField.tsx
+++ src/components/ui/SelectField/SelectField.tsx
@@ -28,6 +28,42 @@ export const SelectField: React.FC<SelectFieldProps> = ({
 }) => {
   return (
     <div className={`${className} relative mr-4 mt-10 md:mt-0`}>
+      <div className="relative w-full">
+        <div className="relative">
+          <select
+            {...register(name)}
+            id={name}
+            className={`peer h-10 w-full border-b-2 border-gray-100 bg-white text-gray-900 placeholder-transparent focus:outline-none focus:border-blue-500 pr-12 appearance-none ${
+              errors[name] ? "border-red-500" : ""
+            }`}
+            style={{
+              backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="gray" width="16px" height="16px"><path d="M7 10l5 5 5-5z"></path></svg>')`,
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: showInfoIcon
+                ? "right 20px center"
+                : "right 0px center",
+              backgroundSize: "30px 30px",
+            }}
+          >
+            <option value="">{defaultValue}</option>
+            {options.map((option, index) => (
+              <option key={index} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+
+          {showInfoIcon && tooltipText && (
+            <div
+              className="absolute top-1/2 right-0 -translate-y-1/2 z-10"
+              style={{ pointerEvents: "auto" }}
+            >
+              <InfoIcon tooltipText={tooltipText} />
+            </div>
+          )}
+        </div>
+      </div>
+
       <label
         htmlFor={name}
         className="absolute left-0 -top-3.5 text-gray-400 text-xs transition-all duration-200 
@@ -35,28 +71,7 @@ export const SelectField: React.FC<SelectFieldProps> = ({
         peer-focus:-top-3.5 peer-focus:text-xs"
       >
         {label}
-        {showInfoIcon && tooltipText && (
-          <span className="ml-2 inline-block">
-            <InfoIcon tooltipText={tooltipText} />
-          </span>
-        )}
       </label>
-
-      <select
-        {...register(name)}
-        id={name}
-        defaultValue={defaultValue}
-        className={`peer h-10 w-full border-b-2 border-gray-100 bg-white text-gray-900 placeholder-transparent focus:outline-none focus:border-blue-500 ${
-          errors[name] ? "border-red-500" : ""
-        }`}
-      >
-        <option value="">{defaultValue}</option>
-        {options.map((option, index) => (
-          <option key={index} value={option}>
-            {option}
-          </option>
-        ))}
-      </select>
 
       {errors[name] && (
         <p className="text-red-500 text-xs mt-1">
